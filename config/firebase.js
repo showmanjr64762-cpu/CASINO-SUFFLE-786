8c564ff5d3677dff0e9f0a4aa20cd6ef00caaab5
showmanjr64762-cpu <showmanjr64762@gmail.com>
2026-03-10 04:20:51 +0500

    Fix firebase private key for Render

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,18 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
-// Fix newline issue in private key
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
+// Construct service account from environment variables
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FB_PROJECT_ID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID, // optional if you want
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FB_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://nj777-2756c-default-rtdb.firebaseio.com"
+  databaseURL: process.env.FB_DATABASE_URL,
 });
 
-module.exports = admin;
+const db = admin.database();
+module.exports = db;
